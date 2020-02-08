9c956140c87c6392d6fe3ecf8ffc03c5570a904c
dionixs <dionixs@tutanota.com>
2020-02-08 17:44:10 +0300

    add first working version

---
+++ main.rb
@@ -0,0 +1,50 @@
+require 'byebug'
+
+require 'mechanize'
+require 'net/http'
+require 'json'
+
+require_relative 'lib/wallhaven'
+
+
+# чтение конфигурации из файла settings.json
+wallhaven = Wallhaven.read_config('settings.json')
+
+# номер текущей страницы
+page = 1
+
+# массив ссылок на картинки
+urls_images = []
+
+# условие выхода из цикла
+condition = nil
+
+loop do
+  puts "Parsing..."
+
+  url = wallhaven.update_url_parameters(page)
+
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  data = JSON.parse(response)
+
+    data.each do |key, value|
+      if key == "data"
+        condition = 0 if value == []
+          value.each do |item|
+            urls_images << item["path"]
+          end
+      end
+    end
+
+  break if condition == 0
+
+  page += 1
+end
+
+Wallhaven.download_image(urls_images)
+
+
+
+
+
