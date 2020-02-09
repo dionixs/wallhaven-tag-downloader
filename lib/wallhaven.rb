b428cbb5a76ac1255ab66640825bbf55a142ee2a
dionixs <dionixs@tutanota.com>
2020-02-09 18:24:52 +0300

    updated version

--- lib/wallhaven.rb
+++ lib/wallhaven.rb
@@ -1,36 +1,59 @@
 # frozen_string_literal: true
 
 class Wallhaven
-  attr_accessor :base_url, :api, :resolution, :tags
+  attr_accessor :base_url, :api, :resolution, :tag, :urls_images
 
   def initialize(params)
     @base_url = params['base_url']
     @api = params['api']
     @resolution = params['resolution']
     @tag = params['tag']
-    # @urls_images = []
+    @page = 0
+    @urls_images = []
   end
 
   def self.read_config(path_to_config)
+    abort "Файл #{path_to_config} не найден!" unless File.exist?(path_to_config)
+
     data = File.read(path_to_config, encoding: 'UTF-8')
     data_json = JSON.parse(data)
     new(data_json)
   end
 
-  def update_url_parameters(page)
+  def query_search
     query = "&categories=111&purity=110&atleast=#{@resolution}&sorting=relevance&order=desc"
     token = "/apikey=#{@api}"
-    url = @base_url + "q=#{@tag}" + query + token + "&page=#{page}"
+    @base_url + "q=#{@tag}" + query + token
   end
 
-  def self.download_image(urls_images)
-    Dir.mkdir 'images' unless File.exist? 'images'
+  def next_page
+    @page += 1
+    current_page = query_search + "&page=#{@page.to_s}"
+  end
+
+  def get_response(url)
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    return 0 if response.include?('Too Many Requests')
+    JSON.parse(response)
+  end
+
+  def parse(data)
+    data['data'].each do |item|
+      @urls_images << item['path']
+    end
+
+    return 0 if data['data'].empty?
+  end
+
+  def self.download_image(directory_name, urls_images)
+    Dir.mkdir("images") unless File.exists?("images")
 
     puts 'Download images...'
 
     urls_images.each do |url|
       agent = Mechanize.new
-      agent.get(url).save "images/#{File.basename(url)}"
+      agent.get(url).save "images/#{directory_name}/#{File.basename(url)}"
     end
   end
 end
--- main.rb
+++ main.rb
@@ -8,39 +8,19 @@ require 'json'
 
 require_relative 'lib/wallhaven'
 
-# чтение конфигурации из файла settings.json
 wallhaven = Wallhaven.read_config('settings.json')
-
-# номер текущей страницы
-page = 1
-
-# массив ссылок на картинки
-urls_images = []
-
-# условие выхода из цикла
-condition = nil
 
 puts "Parsing..."
 
 loop do
-  url = wallhaven.update_url_parameters(page)
+  url = wallhaven.next_page
+  data = wallhaven.get_response(url)
+  break if data == 0
 
-  uri = URI(url)
-  response = Net::HTTP.get(uri)
-  data = JSON.parse(response)
-
-  data.each do |key, value|
-    if key == "data"
-      condition = 0 if value == []
-        value.each do |item|
-          urls_images << item['path']
-        end
-      end
-  end
-
-  break if condition == 0
-
-  page += 1
+  result = wallhaven.parse(data)
+  break if result == 0
 end
 
-Wallhaven.download_image(urls_images)
+Wallhaven.download_image(wallhaven.tag, wallhaven.urls_images)
+
+
